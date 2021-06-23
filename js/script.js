2d495163e70e242a4b163faefb4a534f659e24a3
dbarrera <dbarrerapalacios@gmail.com>
2021-06-23 10:38:54 -0500

    se agregaron los archivos de thee al repositorio

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
-import * as THREE from "https://threejsfundamentals.org/threejs/resources/threejs/r127/build/three.module.js";
-import { OrbitControls } from "https://threejsfundamentals.org/threejs/resources/threejs/r127/examples/jsm/controls/OrbitControls.js";
-import { GLTFLoader } from "https://threejsfundamentals.org/threejs/resources/threejs/r127/examples/jsm/loaders/GLTFLoader.js";
+import * as THREE from "./three.module.js";
+import { OrbitControls } from "./OrbitControls.js";
+import { GLTFLoader } from "./GLTFLoader.js";
 var model;
 
 function main() {
